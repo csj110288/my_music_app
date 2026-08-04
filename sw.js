5d0acb977d6fe6196f20a36d47996cb4dc6c6367
csj110288 <sarampower11@gamil.com>
2026-08-04 14:37:17 +0900

    캐시 강제 초기화 및 v3.0 엔진 적용

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = 'copy-app-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://unpkg.com/wavesurfer.js@7',
-  'https://unpkg.com/wavesurfer.js@7/dist/plugins/regions.min.js'
-];
+// 서비스 워커 캐시 강제 삭제 및 업데이트 버전
+const CACHE_NAME = 'copy-app-v3-reset';
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => caches.delete(cache))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  // 캐시를 거치지 않고 항상 네트워크에서 최신 파일 가져오기
+  event.respondWith(fetch(event.request));
 });
